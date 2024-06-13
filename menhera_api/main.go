22fcf148104202450c2f7d5dbb152f0b87437de0
shimizu <noon3m.op.sh@gmail.com>
2024-06-13 15:05:25 +0900

    test

--- menhera_api/main.go
+++ menhera_api/main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Panicln("読み込み出来ませんでした: %v", err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	loadEnv()
+	server := gin.Default()
 
+	server.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	// envファイルの読み込み
+	fmt.Println("Hello, World!")
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage("Hello, World!")
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+	server.Run(":3012")
 }
