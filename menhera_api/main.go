a0a633b104265fa6e0e30e2cb764dfe9e3c02085
shimizu <noon3m.op.sh@gmail.com>
2024-06-14 11:05:12 +0900

    [update] jwtを保留 [add] スタンプ対応

--- menhera_api/main.go
+++ menhera_api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -25,19 +26,28 @@ func main() {
 	var bot *linebot.Client
 	var err error
 
-	jwt, err := Jwt()
+	// jwt, err := Jwt()
+	fmt.Println("30")
 	if err != nil {
 		bot, err = linebot.New(
 			os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 			os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
 		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
 	} else {
 		bot, err = linebot.New(
 			os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-			os.Getenv(jwt),
+			os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
 		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// return
 	}
-
+	fmt.Println("50")
 	server.GET("/", func(c *gin.Context) {
 
 		c.JSON(200, gin.H{
@@ -46,18 +56,15 @@ func main() {
 	})
 
 	server.POST("/callback", func(c *gin.Context) {
+		fmt.Println("59")
 		events, err := bot.ParseRequest(c.Request)
 		if err != nil {
 			if err == linebot.ErrInvalidSignature {
 				log.Print(err)
-				c.Status(http.StatusBadRequest)
-			} else {
-				log.Print(err)
-				c.Status(http.StatusInternalServerError)
 			}
 			return
 		}
-
+		fmt.Println("67")
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
@@ -68,10 +75,17 @@ func main() {
 						log.Print(err)
 						aiResponse = linebot.TextMessage{Text: message.Text}
 					}
-
+					fmt.Println("78")
 					// aiResponseのTextフィールドの内容を新しいテキストメッセージとして送信
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(aiResponse.Text)).Do(); err != nil {
 						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					log.Print("Received StickerMessage")
+					// スタンプメッセージに対する返信
+					replyMessage := linebot.NewTextMessage("スタンプを送らないで...")
+					if _, err := bot.ReplyMessage(event.ReplyToken, replyMessage).Do(); err != nil {
+						log.Print("ReplyMessage Error:", err)
 					}
 				}
 			}
